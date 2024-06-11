331b4c1de9d467c78f16c05f69a69533300ba6fb
Aniket Kumar <aniketas2111@gmail.com>
2024-06-11 16:05:33 +0530

    indexing

---
+++ output/lecture7.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Array- used to store multiple values of the same datatype in single variable";
+    cout<<"5 values are stored"; 
+    cout<<"First index = a[0]";
+    cout<<"Second index = a[1]";
+}
