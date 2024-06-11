0d14e5bcb0ee96d7edab4e70a80326a694faf92a
Aniket Kumar <aniketas2111@gmail.com>
2024-06-11 15:42:42 +0530

    and operater

---
+++ output/lecture5que1.cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a , b ,c ,d;
+    cout<<"Enter 4 integer a,b,c,d"<<endl;
+    cin>> a ;
+    cin>>b;
+    cin >> c ;
+    cin>>d;
+    cout<<(a>b && c>d);
+
+}
